3536a6665142f22a307bc5632fb2bd3762c348aa
Philippe Charles <philippe.charles@nbb.be>
2015-12-11 11:05:30 +0100

    Replaced TsFilter with TsCrop and TsClean.

---
+++ src/main/java/demetra/cli/various/TsCrop.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2015 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package demetra.cli.various;
+
+import com.google.common.annotations.VisibleForTesting;
+import demetra.cli.helpers.BasicArgsParser;
+import demetra.cli.helpers.BasicCliLauncher;
+import demetra.cli.helpers.InputOptions;
+import static demetra.cli.helpers.ComposedOptionSpec.newInputOptionsSpec;
+import static demetra.cli.helpers.ComposedOptionSpec.newOutputOptionsSpec;
+import static demetra.cli.helpers.ComposedOptionSpec.newStandardOptionsSpec;
+import demetra.cli.helpers.OutputOptions;
+import demetra.cli.helpers.StandardOptions;
+import demetra.xml.TsPeriodSelectorAdapter;
+import ec.tss.TsCollectionInformation;
+import ec.tss.xml.XmlTsCollection;
+import ec.tstoolkit.timeseries.Day;
+import ec.tstoolkit.timeseries.PeriodSelectorType;
+import ec.tstoolkit.timeseries.TsPeriodSelector;
+import java.util.Date;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import demetra.cli.helpers.BasicCommand;
+import demetra.cli.helpers.ComposedOptionSpec;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+public final class TsCrop implements BasicCommand<TsCrop.Parameters> {
+
+    public static void main(String[] args) {
+        BasicCliLauncher.run(args, Parser::new, TsCrop::new, o -> o.so);
+    }
+
+    public static final class Parameters {
+
+        StandardOptions so;
+        public InputOptions input;
+        @XmlJavaTypeAdapter(TsPeriodSelectorAdapter.class)
+        public TsPeriodSelector periodSelector;
+        public OutputOptions output;
+    }
+
+    @Override
+    public void exec(Parameters params) throws Exception {
+        TsCollectionInformation result = params.input.readValue(XmlTsCollection.class);
+        if (params.periodSelector.getType() != PeriodSelectorType.All) {
+            selectPeriods(result, params.periodSelector);
+        }
+        params.output.writeValue(XmlTsCollection.class, result);
+    }
+
+    @VisibleForTesting
+    static void selectPeriods(TsCollectionInformation info, TsPeriodSelector selector) {
+        info.items.forEach(o -> {
+            o.data = o.hasData() ? o.data.select(selector) : null;
+        });
+    }
+
+    @VisibleForTesting
+    static final class Parser extends BasicArgsParser<Parameters> {
+
+        private final ComposedOptionSpec<StandardOptions> so = newStandardOptionsSpec(parser);
+        private final ComposedOptionSpec<InputOptions> input = newInputOptionsSpec(parser);
+        private final ComposedOptionSpec<TsPeriodSelector> periodSelector = new PeriodSelectorOptionsSpec(parser);
+        private final ComposedOptionSpec<OutputOptions> output = newOutputOptionsSpec(parser);
+
+        @Override
+        protected Parameters parse(OptionSet o) {
+            Parameters result = new Parameters();
+            result.input = input.value(o);
+            result.periodSelector = periodSelector.value(o);
+            result.output = output.value(o);
+            result.so = so.value(o);
+            return result;
+        }
+    }
+
+    private static final class PeriodSelectorOptionsSpec implements ComposedOptionSpec<TsPeriodSelector> {
+
+        private final OptionSpec<Date> from;
+        private final OptionSpec<Date> to;
+        private final OptionSpec<Integer> first;
+        private final OptionSpec<Integer> last;
+
+        public PeriodSelectorOptionsSpec(OptionParser p) {
+            this.from = p.accepts("from").withRequiredArg().ofType(Date.class);
+            this.to = p.accepts("to").withRequiredArg().ofType(Date.class);
+            this.first = p.accepts("first").withRequiredArg().ofType(Integer.class);
+            this.last = p.accepts("last").withRequiredArg().ofType(Integer.class);
+        }
+
+        @Override
+        public TsPeriodSelector value(OptionSet o) {
+            TsPeriodSelector result = new TsPeriodSelector();
+            if (o.has(from)) {
+                if (o.has(to)) {
+                    result.between(new Day(from.value(o)), new Day(to.value(o)));
+                } else {
+                    result.from(new Day(from.value(o)));
+                }
+            } else if (o.has(to)) {
+                result.to(new Day(to.value(o)));
+            } else if (o.has(first)) {
+                if (o.has(last)) {
+                    result.excluding(first.value(o), last.value(o));
+                } else {
+                    result.first(first.value(o));
+                }
+            } else if (o.has(last)) {
+                result.last(last.value(o));
+            } else {
+                result.all();
+            }
+            return result;
+        }
+    }
+}
