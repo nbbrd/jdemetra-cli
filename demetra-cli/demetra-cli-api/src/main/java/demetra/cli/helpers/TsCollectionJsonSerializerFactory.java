479812658199e5806080b167c5a817614e291e26
Philippe Charles <philippe.charles@nbb.be>
2016-05-25 15:38:44 +0200

    Added specific media types.

---
+++ demetra-cli/demetra-cli-api/src/main/java/demetra/cli/helpers/TsCollectionJsonSerializerFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
+ */
+package demetra.cli.helpers;
+
+import be.nbb.cli.util.Serializer;
+import be.nbb.cli.util.SerializerFactory;
+import be.nbb.cli.util.SerializerFactoryAlias;
+import com.google.common.net.MediaType;
+import static demetra.cli.helpers.DemetraMediaTypes.TS_COLLECTION_JSON;
+import ec.tss.xml.XmlTsCollection;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Philippe Charles
+ */
+@ServiceProvider(service = SerializerFactory.class)
+public final class TsCollectionJsonSerializerFactory implements SerializerFactory {
+
+    private final SerializerFactoryAlias alias = SerializerFactoryAlias.of(XmlTsCollection.class, TS_COLLECTION_JSON, MediaType.JSON_UTF_8);
+
+    @Override
+    public boolean canHandle(MediaType mediaType, Class<?> type) {
+        return alias.canHandle(mediaType, type, Lookup.getDefault().lookupAll(SerializerFactory.class));
+    }
+
+    @Override
+    public <X> Serializer<X> create(Class<X> type, boolean formattedOutput) {
+        return alias.create(type, formattedOutput, Lookup.getDefault().lookupAll(SerializerFactory.class));
+    }
+}
